c7d75847b453af4f26518c4ff32d5d5fe67948fa
Marsay Febrianto <222112177@stis.ac.id>
2024-06-14 11:19:39 +0700

    fiks deploy

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -13,7 +13,7 @@ const LandingPage = () => {
     return (
         <section
             id="home"
-            className="relative z-10 overflow-hidden bg-white pb-16 md:px-44 px-8 pt-[100px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+            className="relative z-10 overflow-hidden pb-16 md:px-44 px-8 pt-[100px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
             style={{
                 backgroundImage: `url('/hero2.jpg')`,
                 backgroundSize: 'cover',
@@ -22,7 +22,7 @@ const LandingPage = () => {
         >
             <div className="absolute inset-0 bg-shade4 opacity-80"></div>
             <div className="container relative w-full z-20 max-w-[1080px] mx-auto my-0" >
-                <h1 className="mb-5 text-white text-heading1">
+                <h1 className="mb-5 text-white text-5xl md:text-8xl font-sans font-semibold">
                     Pusat Informasi dan Layanan Kementrian Pertanian
                 </h1>
                 <div className='mt-14'>
