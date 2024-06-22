9a4b73d17c4ee3197d7965aa6401055e911ed6bf
xthaj <athajuli@gmail.com>
2024-06-22 17:50:02 +0700

    halaman tabel ! filter blm sama widthnya - layout nyontek magazine? kayaknya - tambah data

--- app/basis-data/page.tsx
+++ app/basis-data/page.tsx
@@ -1,178 +1,311 @@
 "use client"
+import Judul2 from '@/components/Tittle/judul2';
+import {Cascader, CascaderProps, Divider, Image, Table, TableColumnsType} from 'antd';
 import React from 'react';
-import { Chart } from 'react-google-charts';
+import { Select, DatePicker, Button } from 'antd';
+import {AlamatType, BasdatType, DipaProps} from "@/utils/dataType";
+import Link from "next/link";
+import {LinkOutlined} from "@ant-design/icons";
+import Judul3 from "@/components/Tittle/judul3";
+import {dataAlamat} from "@/utils/dataAlamat";
+import {dataBasdat} from "@/utils/dataBasdat";
+import dayjs from "dayjs";
 
-const ChartComponent = () => {
-    const data_map = [
-        ['Provinsi', 'Produksi Padi (ton)'],
-        ['Aceh', 1646941.44],
-        ['Sumatera Utara', 2068481.33],
-        ['Sumatera Barat', 1421092.02],
-        ['Riau', 229653.87],
-        ['Jambi', 321871.07],
-        ['Sumatera Selatan', 2750155.69],
-        ['Bengkulu', 286254.72],
-        ['Lampung', 2539088.6],
-        ['Bangka-Belitung', 58374.15],
-        ['Kepulauan Riau', 797.71],
-        [{v:'ID-JK', f:'Jakarta'}, 3510.65],
-        ['Jawa Barat', 9248606.09],
-        ['Jawa Tengah', 9617269.44],
-        ['Yogyakarta', 537358.85],
-        ['Jawa Timur', 9792575],
-        ['Banten', 1648628.27],
-        ['Bali', 622328.03],
-        ['Nusa Tenggara Barat', 1431792.21],
-        [{v:'ID-NT', f:'Nusa Tenggara Timur'}, 781903.75],
-        [{v:'ID-KB', f:'Kalimantan Barat'}, 761528.78],
-        ['Kalimantan Tengah', 412028.62],
-        ['Kalimantan Selatan', 903540.61],
-        ['Kalimantan Timur', 248350.36],
-        [{v:'ID-KU', f:'Kalimantan Utara'}, 32683],
-        ['Sulawesi Utara', 262375.64],
-        ['Sulawesi Tengah', 832820.11],
-        ['Sulawesi Selatan', 5173740.18],
-        ['Sulawesi Tenggara', 513291.82],
-        ['Gorontalo', 242389.64],
-        ['Sulawesi Barat', 319619.22],
-        [{v:'ID-MA', f:'Maluku'}, 102382.33],
-        ['Maluku Utara', 34929.27],
-        [{v:'ID-PB', f:'Papua Barat'}, 25457.78],
-        ['Papua', 216901.27]
-    ];
+const { Option } = Select;
+const { RangePicker } = DatePicker;
 
-    const options_map = {
-        region: 'ID',
-        displayMode: 'regions',
-        resolution: 'provinces',
-        colorAxis: {
-            colors: ['#ff4c4c', '#FAB733', '#69B34C'], // Red to Yellow to Green
-            minValue: 797.71, // Minimum value for the color gradient
-            maxValue: 9792575, // Maximum value for the color gradient (adjust as needed)
-        },
-        backgroundColor: '#f2f2f2',
-        datalessRegionColor: '#ffffff',
-        defaultColor: '#f5f5f5',
-        width: '100%',
-        height: '100%',
-    };
+interface Option {
+    value: string;
+    label: string;
+    children?: Option[];
+}
 
-    const data_bar = [
-        ['Tahun', 'Produksi (ton)'],  // Adding headers to match Google Charts format
-        ['2018', 59200533.72],
-        ['2019', 54604033.34],
-        ['2020', 54649202.24],
-        ['2021', 53776236.17],
-        ['2022', 54338410.44],
-        ['2023', 53963913.2]
-    ];
+// Options buat indikator
+const options: Option[] = [
+    {
+        value: 'tanaman_pangan',
+        label: 'Tanaman Pangan',
+        children: [
+            {
+                value: 'jagung',
+                label: 'Jagung',
+            },
+            {
+                value: 'kacang_hijau',
+                label: 'Kacang Hijau',
+            },
+            {
+                value: 'kacang_tanah',
+                label: 'Kacang Tanah',
+            },
+        ],
+    },
+    {
+        value: 'hortikultura',
+        label: 'Hortikultura',
+        children: [
+            {
+                value: 'adenium',
+                label: 'Adenium',
+            },
+            {
+                value: 'aglaonema',
+                label: 'Aglaonema',
+            },
+            {
+                value: 'alpukat',
+                label: 'Alpukat',
+            },
+        ],
+    },
+    {
+        value: 'perkebunan',
+        label: 'Perkebunan',
+        children: [
+            {
+                value: 'cengkeh',
+                label: 'Cengkeh',
+            },
+            {
+                value: 'jambu_mete',
+                label: 'Jambu Mete',
+            },
+            {
+                value: 'kakao',
+                label: 'Kakao',
+            },
+        ],
+    },
+    {
+        value: 'peternakan',
+        label: 'Peternakan',
+        children: [
+            {
+                value: 'ayam_buras',
+                label: 'Ayam Buras',
+            },
+            {
+                value: 'ayam_ras_pedaging',
+                label: 'Ayam Ras Pedaging',
+            },
+            {
+                value: 'babi',
+                label: 'Babi',
+            },
 
-    const options_bar = {
-        title: 'Produksi Beras Indonesia (ton) per Tahun',
-        hAxis: {
-            title: 'Tahun'
-        },
-        vAxis: {
-            title: 'Produksi (ton)',
-            minValue: 0  // Ensure y-axis starts at 0
-        },
-        bar: {groupWidth: "95%"},
-    };
+        ],
+    },
+];
 
-    const data_line = [
-        ['Tahun', 'Produksi (ton)'],  // Adding headers to match Google Charts format
-        ['2018', 59200533.72],
-        ['2019', 54604033.34],
-        ['2020', 54649202.24],
-        ['2021', 53776236.17],
-        ['2022', 54338410.44],
-        ['2023', 53963913.2]
-    ];
+// options buat level
+const options2: Option[] = [
+    {
+        value: 'nasional',
+        label: 'Nasional',
+        children: [
+            {
+                value: 'provinsi',
+                label: 'Provinsi',
+                children: [
+                    {
+                        value: 'jakarta',
+                        label: 'DKI Jakarta',
+                        children: [
+                            {
+                                value: 'kepseribu',
+                                label: 'Kab. Kepulauan Seribu',
+                            },
+                            {
+                                value: 'jakbar',
+                                label: 'Kota Jakarta Barat',
+                            },
+                            {
+                                value: 'jakpus',
+                                label: 'Kota Jakarta Pusat',
+                            },
+                            {
+                                value: 'jaksel',
+                                label: 'Kota Jakarta Selatan',
+                            },
+                        ],
+                    },
+                    {
+                        value: 'jawabarat',
+                        label: 'Jawa Barat',
+                        children: [
+                            {
+                                value: 'bandung',
+                                label: 'Kota Bandung',
+                            },
+                            {
+                                value: 'bekasi',
+                                label: 'Kota Bekasi',
+                            },
+                            {
+                                value: 'depok',
+                                label: 'Kota Depok',
+                            },
+                            {
+                                value: 'bogor',
+                                label: 'Kab. Bogor',
+                            },
+                        ],
+                    },
+                    {
+                        value: 'jawatimur',
+                        label: 'Jawa Timur',
+                        children: [
+                            {
+                                value: 'surabaya',
+                                label: 'Kota Surabaya',
+                            },
+                            {
+                                value: 'malang',
+                                label: 'Kota Malang',
+                            },
+                            {
+                                value: 'sidoarjo',
+                                label: 'Kab. Sidoarjo',
+                            },
+                            {
+                                value: 'gresik',
+                                label: 'Kab. Gresik',
+                            },
+                        ],
+                    },
+                ],
+            }
+        ],
+    }
+];
 
-    const options_line = {
-        title: 'Produksi Beras Indonesia (ton) per Tahun',
-        hAxis: {
-            title: 'Tahun'
-        },
-        vAxis: {
-            title: 'Produksi (ton)',
-            minValue: 0  // Ensure y-axis starts at 0
-        }
-        // bar: {groupWidth: "95%"},
-    };
+const onChange: CascaderProps<Option>['onChange'] = (value) => {
+    console.log(value);
+};
 
-    const options_bar2 = {
-        title: 'Produksi Beras Indonesia (ton) per Tahun',
-        hAxis: {
-            title: 'Tahun'
-        },
-        vAxis: {
-            title: 'Produksi (ton)',
-            minValue: 0  // Ensure y-axis starts at 0
-        }
-        // bar: {groupWidth: "95%"},
-    };
+const CascaderWithParentAllowed: React.FC = () => (
+    <Cascader
+        options={options2}
+        onChange={onChange}
+        size="large"
+        defaultValue={['nasional']}
+        changeOnSelect  // This Cascader allows selecting parent options
+    />
+);
 
+const CascaderWithoutParentAllowed: React.FC = () => (
+    <Cascader
+        options={options}
+        onChange={onChange}
+        size="large"
+        defaultValue={['tanaman_pangan']}
+        // No changeOnSelect prop provided, default behavior is leaf node selection only
+    />
+);
 
+const columns: TableColumnsType<BasdatType> = [
+    {
+        title: 'Komoditas',
+        dataIndex: 'komoditas',
+    },
+    {
+        title: 'Satuan',
+        dataIndex: 'satuan',
+    },
+    {
+        title: '2020',
+        dataIndex: '2020',
+    },
+    {
+        title: '2021',
+        dataIndex: '2021',
+    },
+    {
+        title: '2022',
+        dataIndex: '2022',
+    },
+    {
+        title: '2023',
+        dataIndex: '2023',
+    },
+    {
+        title: '2024',
+        dataIndex: '2024',
+    },
+];
 
+const handleResetFilter = () => {
+    window.location.reload();
+};
+
+const handleSearchData = () => {
+    // Alert function for "Cari Data" button
+    alert('Preview - fitur cari data yang sebenarnya belum diimplementasikan.');
+};
+
+const BasisDataPage: React.FC = () => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <div style={{ width: '90%', height: '400px', marginBottom: '50px' }}>
-                <Chart
-                    chartType="GeoChart"
-                    data={data_map}
-                    options={options_map}
-                    width="100%"
-                    height="100%"
-                    legendToggle
-                />
+        <div>
+            <header >
+                <h2 className="text-heading2 md:text-heading1 mb-2">Basis Data Statistik Pertanian</h2>
+            </header>
+            <Divider style={{ borderTop: '4px solid #8A8817' }} />
+
+            <div className="space-y-4">
+                <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 w-full">
+                    <div className="flex flex-grow items-center space-x-2 md:w-1/3">
+                        <label className="text-lg font-semibold">Atur data berdasarkan</label>
+                        <Select size="large" style={{ minWidth: 150 }} defaultValue="komoditas">
+                            <Option value="komoditas">Komoditas</Option>
+                            <Option value="indikator">Indikator</Option>
+                            <Option value="lokasi">Lokasi</Option>
+                        </Select>
+                    </div>
+                </div>
             </div>
 
-            // ini emang rada aneh tp di dokumentasi kalo side by side pake table
+            <div className="space-y-4">
+                <div className="flex flex-col md:flex-row md:space-x-4 space-y-4 md:space-y-0 w-full">
 
-            <table className="columns">
-                <tr>
-                    <td>
-                        <div style={{ width: '90%', height: '400px' }}>
-                            <Chart
-                                chartType="ColumnChart"
-                                data={data_bar}
-                                options={options_bar}
-                                width="100%"
-                                height="100%"
-                                legendToggle
-                            />
-                        </div>
-                    </td>
-                    <td>
-                        <div style={{ width: '90%', height: '400px' }}>
-                            <Chart
-                                chartType="LineChart"
-                                data={data_line}
-                                options={options_line}
-                                width="100%"
-                                height="100%"
-                                legendToggle
-                            />
-                        </div>
-                    </td>
-                </tr>
-            </table>
+                    <div className="flex flex-col flex-grow md:w-1/3">
+                        <label className="mb-2 text-lg font-semibold">Indikator</label>
+                        <CascaderWithoutParentAllowed />
+                    </div>
 
-            <div style={{ width: '90%', height: '400px', marginBottom: '50px' }}>
-                <Chart
-                    chartType="BarChart"
-                    data={data_map}
-                    options={options_bar2}
-                    // width="100%"
-                    // height="100%"
-                    legendToggle
-                />
+                    <div className="flex flex-col flex-grow md:w-1/3">
+                        <label className="mb-2 text-lg font-semibold">Level</label>
+                        <CascaderWithParentAllowed />
+                    </div>
+
+                    <div className="flex flex-col flex-grow md:w-1/3">
+                        <label className="mb-2 text-lg font-semibold">Tanggal</label>
+                        <RangePicker picker="year" size='large' defaultValue={[
+                            dayjs('2020-01-01', 'YYYY-MM-DD'),
+                            dayjs('2024-01-01', 'YYYY-MM-DD')
+                        ]}/>
+                    </div>
+
+                </div>
+                <div className="flex justify-end ">
+                    <div className='flex space-x-4'>
+                        <Button size="large" onClick={handleResetFilter}>Reset Filter</Button>
+                        <Button type="primary" size="large" onClick={handleSearchData}>Cari Data</Button>
+                    </div>
+                </div>
             </div>
 
-
+            <div id='alamat1' className='w-auto'>
+                <Judul3 text='Luas Panen Tanaman Pangan Nasional berdasarkan Komoditas (2020-2024)' />
+                <Table
+                    columns={columns}
+                    dataSource={dataBasdat}
+                    scroll={{ x: true }}
+                    pagination={false}
+                />
+                <Divider />
+            </div>
         </div>
     );
 };
 
-export default ChartComponent;
+export default BasisDataPage;
